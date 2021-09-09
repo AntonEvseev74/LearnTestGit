31d4484963f542dcc9a303248420978f98ed7f2b
Anton Evseev <eavae@mail.ru>
2021-09-09 21:07:30 +0500

    2й коммит на ветку dev

--- app/src/main/java/ru/evant/learntestgit/MainActivity.java
+++ app/src/main/java/ru/evant/learntestgit/MainActivity.java
@@ -3,12 +3,27 @@ package ru.evant.learntestgit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnBegin;
+    TextView tvBegin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        btnBegin = (Button) findViewById(R.id.btnBegin);
+        tvBegin = (TextView) findViewById(R.id.tvBegin);
+    }
+
+    public void onClick(View view) {
+        tvBegin.setText(" It`s Begin!");
     }
 }
